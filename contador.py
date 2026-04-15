41777b7fcbe8bc2b0f17294db178d88c6c20eaee
Leonar Heredia <leonarheredia857@gmail.com>
2026-04-15 11:48:25 +0200

    Introduce bug en siguiente (suma 2)

--- contador.py
+++ contador.py
@@ -1,12 +1,2 @@
-# contador.py
-# Versión inicial correcta
-
 def siguiente(n):
-    return n + 1
-
-if __name__ == "__main__":
-    import sys
-    valor = int(sys.argv[1])
-    print(siguiente(valor))
-# Log: añadido comentario 1
-# Log: añadido comentario 2
+    return n + 2 # BUG intencionado: debería ser +1
